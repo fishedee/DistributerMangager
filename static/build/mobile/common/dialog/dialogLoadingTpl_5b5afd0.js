c56241cd1c7ab8ac4cf58d2837b4c0651e30dd86
fishedee <306766045@qq.com>
2015-03-06 21:19:22 +0800

    修复bug，以及去除static/build目录

--- static/build/mobile/common/dialog/dialogLoadingTpl_5b5afd0.js
+++
@@ -1,7 +0,0 @@
-function(obj){
-var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
-with(obj||{}){
-__p+='<div id="common_dialog_loading">\n</div>';
-}
-return __p;
-}
--- static/build/mobile/common/shopcart/shopcartTpl_1f286b8.js
+++
@@ -1,7 +0,0 @@
-function(obj){
-var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
-with(obj||{}){
-__p+='<a href="shopcart.html">\n\t<div id="common_shopcart">\n\t\t<span class="icon"></span>\n\t\t<span class="text">购物车</span>\n\t</div>\n</a>';
-}
-return __p;
-}
