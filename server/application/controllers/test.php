5ebf6d3251a5f977b7181f231ebf3f6bafad6a0f
Shi Yang <kid7st@gmail.com>
2015-03-15 21:34:10 +0800

    distributionOrder测试接口

---
+++ server/application/controllers/test.php
@@ -0,0 +1,77 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test extends CI_Controller
+{
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('distribution/distributionorderAo', 'distributionOrderAo');
+        $this->load->library('argv', 'argv');
+    }
+
+    /**
+     * @view json
+     */
+    public function add(){
+        //检查参数
+        $data = $this->argv->checkPost(array(
+            array('upUserId', 'require'),
+            array('downUserId', 'require'),
+            array('shopOrderId', 'require'),
+            array('price', 'require'),
+            array('state', 'require')
+        ));
+            
+        $upUserId = $data['upUserId'];
+        $downUserId = $data['downUserId'];
+        unset($data['upUserId']);
+        unset($data['downUserId']);
+        $this->distributionOrderAo->add($upUserId, $downUserId, $data);
+    }
+
+    /**
+     * @view json
+     */
+    public function mod(){
+        $data = $this->argv->checkPost(array(
+            array('distributionOrderId', 'require'),
+            array('shopOrderId', 'require'),
+            array('price', 'require')
+        ));
+
+        $distributionOrderId = $data['distributionOrderId'];
+        $this->distributionOrderAo->mod($distributionOrderId, $data);
+    }
+
+    /**
+     * @view json
+     */
+    public function payOrder($distributionOrderId){
+        $data = $this->argv->checkPost(array(
+            array('distributionOrderId', 'require')
+        ));  
+
+        $this->distributionOrderAo->payOrder($data['distributionOrderId']);
+    }
+
+    /**
+     * @view json
+     */
+    public function confirm($distributionOrderId){
+        $data = $this->argv->checkPost(array(
+            array('distributionOrderId', 'require')
+        ));  
+
+        $this->distributionOrderAo->confirm($data['distributionOrderId']);
+    }
+
+    /**
+     * @view json
+     */
+    public function del($distributionOrderId){
+        $data = $this->argv->checkPost(array(
+            array('distributionOrderId', 'require')
+        ));
+        $this->distributionAo->del($data['distributionOrderId']);
+    }
+}
+
