a0cb4a9435d0e1fb095c6e2beb7e45b5033327de
milkbobo <mileto@qq.com>
2015-05-30 13:13:04 +0800

    微信接入验证

--- server/index.php
+++ server/index.php
@@ -1,5 +1,28 @@
 <?php
 /**
+ * 微信接入验证
+ * 在入口进行验证而不是放到框架里验证，主要是解决验证URL超时的问题
+ */
+define("TOKEN", "weiyd");
+if (! empty ( $_GET ['echostr'] ) && ! empty ( $_GET ["signature"] ) && ! empty ( $_GET ["nonce"] )) {
+	$signature = $_GET ["signature"];
+	$timestamp = $_GET ["timestamp"];
+	$nonce = $_GET ["nonce"];
+
+	$tmpArr = array (
+			TOKEN,
+			$timestamp,
+			$nonce
+	);
+	sort ( $tmpArr, SORT_STRING );
+	$tmpStr = sha1 ( implode ( $tmpArr ) );
+
+	if ($tmpStr == $signature) {
+		echo $_GET ["echostr"];
+	}
+	exit ();
+}
+/**
  * CodeIgniter
  *
  * An open source application development framework for PHP
